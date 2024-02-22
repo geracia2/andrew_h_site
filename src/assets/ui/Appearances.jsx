97c826a699cf78663c91062e1b4abebea00b5204
Aaron Geraci <geracia2@gmail.com>
2024-02-22 13:34:41 -0500

    setting up first parts of the appearances list

---
+++ src/assets/ui/Appearances.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Appearances() {
+    return (
+        <div>
+            appearances list
+            <ul>
+                <li>Bloom Stories</li>
+                <li>WGUC Cincinnati Radio</li>
+                <li>Eko Media</li>
+                <li>Spotify</li>
+                <li>Spoken Layer</li>
+                <li>CABVI</li>
+            </ul>
+        </div>
+    )
+}
