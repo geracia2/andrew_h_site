9af6ce9e9a4643600de72d62c41680e293cacc53
Aaron Geraci <geracia2@gmail.com>
2024-03-04 14:11:05 -0500

    setup custom endpoints

--- src/assets/ui/Nav.jsx
+++ src/assets/ui/Nav.jsx
@@ -1,45 +1,55 @@
-import { Menu, Group, Center, Burger, Container, Image } from '@mantine/core';
+import { Menu, Group, Center, Burger, Container, Image, Button, Text, rem } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons-react';
-import { MantineLogo } from '@mantinex/mantine-logo';
+import {
+  IconSettings,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from '@tabler/icons-react';
 import classes from './Nav.module.css';
+import { Link } from 'react-router-dom';
 
 const links = [
-  { link: '/about', label: 'Features' },
+  { link: '/', label: 'Home' },
   {
     link: '#1',
-    label: 'Learn',
+    label: 'Voice Over',
     links: [
-      { link: '/docs', label: 'Documentation' },
-      { link: '/resources', label: 'Resources' },
-      { link: '/community', label: 'Community' },
-      { link: '/blog', label: 'Blog' },
+      { link: '/commercial', label: 'Commercial' },
+      { link: '/eLearning', label: 'eLearning' },
+      { link: '/eExplainers', label: 'Explainers' },
     ],
   },
-  { link: '/about', label: 'About' },
-  { link: '/pricing', label: 'Pricing' },
-  {
-    link: '#2',
-    label: 'Support',
-    links: [
-      { link: '/faq', label: 'FAQ' },
-      { link: '/demo', label: 'Book a demo' },
-      { link: '/forums', label: 'Forums' },
-    ],
-  },
+  { link: '/studio', label: 'Studio' },
+  { link: '/podcast', label: 'Winging It Podcast' },
+  { link: '/resume', label: 'Resume' },
+  { link: '/contact', label: 'Contact' },
+
 ];
 
 export default function Nav() {
+  // burger open/close
   const [opened, { toggle }] = useDisclosure(false);
 
+  // map the links
   const items = links.map((link) => {
+    // if we have fold downs map those with this:
     const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
+      <Menu.Item key={item.link}>
+        <Link to={item.link} className={classes.link}>
+          {item.label}
+        </Link>
+      </Menu.Item>
     ));
 
+    // if we have fold down menus make them the following
     if (menuItems) {
       return (
         <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
+          {/* menu target is the fold down title */}
           <Menu.Target>
             <a
               href={link.link}
@@ -58,30 +68,43 @@ export default function Nav() {
     }
 
     return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
+      <Link to={link.link} key={link.label} className={classes.link}>
         {link.label}
-      </a>
+      </Link>
     );
   });
+
+  const mobileNav = (
+    <Menu shadow="md" width={200} opened={opened} onChange={toggle} position="bottom" offset={50} >
+      <Menu.Dropdown>
+        <Menu.Item>
+          {items}
+        </Menu.Item>
+        
+      </Menu.Dropdown>
+    </Menu>
+  )
 
   return (
     <header className={classes.header}>
       <Container size="md">
+        {/* desktop nav */}
         <div className={classes.inner}>
-          <MantineLogo size={28} />
+          <Image h={90} src='.\images\Logo_1_tansp_white.png' />
           <Group gap={5} visibleFrom="sm">
             {items}
           </Group>
+          {/* mobile nav */}
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          {opened &&
+            <>
+              {/* <div style={{ border: 'solid 5px red' }}> */}
+                {mobileNav}
+              {/* </div> */}
+            </>
+          }
         </div>
       </Container>
     </header>
   );
 }
-
-    //  <Image h={80} src='.\images\Logo_1_tansp_white.png'/>
