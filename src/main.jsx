640f65a5aebbcd0eb6b6130b6b37472eb3490b94
Aaron Geraci <geracia2@gmail.com>
2024-03-06 16:14:24 -0500

    adding in color schemes

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,24 @@ import '@mantine/core/styles.css';
 // import '@mantine/dropzone/styles.css';
 // import '@mantine/code-highlight/styles.css';
 
-import { createTheme, MantineProvider } from '@mantine/core';
-
+import { MantineProvider, createTheme } from '@mantine/core';
+const myColor = [
+  '#ffebff',
+  '#f5d5fc',
+  '#e6a9f3',
+  '#d779eb',
+  '#cb51e4',
+  '#c437e0',
+  '#c029df',
+  '#a91cc6',
+  '#9715b1',
+  '#840a9c'
+];
 const theme = createTheme({
   /** Put your mantine theme override here */
+  colors: {
+    myColor,
+  }
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
